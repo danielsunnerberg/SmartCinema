8171cd843a10adf21cd374ec9ee7abf30dcf7d28
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2016-02-17 21:28:35 +0100

    Renamed index to start

--- routes/start.js
+++ routes/start.js
@@ -4,7 +4,7 @@ var config = require('../config');
 var mediaLibrary = require('../models/MediaLibrary');
 var kodiApiExecutor = require('../models/KodiApiExecutor');
 
-router.get('/', function (req, res, next) {
+router.get('/start', function (req, res, next) {
 
     var library = new mediaLibrary.MediaLibrary(config.share.internal_path);
     var video = library.getFirstVideo(config.valid_file_extensions);
